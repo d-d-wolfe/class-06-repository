ad9a691a9a2066ed967e4a3d0ee5200fa9cecaf7
d-d-wolfe <dave.wolfe623@gmail.com>
2020-05-12 17:25:21 -0700

    completed the weather feature

--- server.js
+++ server.js
@@ -33,16 +33,16 @@ app.get('/location', getLocation);
   //resp.send(currentLocation);
 //});
 
-app.get('/weather', (req, resp) => {
-  const weatherInfo = require('./data/weather.json');
-  const theForcast = [];
-  let forcastStats = weatherInfo.data;
-  forcastStats.map(obj => {
-    let weatherResult = new Weather(obj);
-    theForcast.push(weatherResult);
-  });
-  resp.send(theForcast);
-});
+app.get('/weather', getWeather);
+//   const weatherInfo = require('./data/weather.json');
+//   const theForcast = [];
+//   let forcastStats = weatherInfo.data;
+//   forcastStats.map(obj => {
+//     let weatherResult = new Weather(obj);
+//     theForcast.push(weatherResult);
+//   });
+//   resp.send(theForcast);
+// });
 
 function getLocation(request, response) {
  // console.log(request, response);
@@ -59,12 +59,40 @@ function getLocation(request, response) {
   .query(queryParams)
   .then(resultFromLocationIQ => {
     const newLocation = new Location(resultFromLocationIQ.body[0]);
-    console.log(newLocation);
+    //console.log(newLocation);
     response.send(newLocation);
 
   })
 }
 
+function getWeather(request, response) {
+  // console.log(request, response);
+   const weatherResult = request.query.weather;
+   const urlOfApi = 'https://api.weatherbit.io/v2.0/forecast/daily';
+ 
+   const weatherParams = {
+     lat: request.query.latitude,
+     lon: request.query.longitude,
+     key: process.env.WEATHER_API_KEY,
+     format: 'json'
+   };
+
+   const forecast = [];
+ 
+   superagent.get(urlOfApi)
+   .query(weatherParams)
+   .then(resultFromWeatherBit => {
+    let forecastStats = resultFromWeatherBit.body.data;
+    let result = forecastStats.map(obj => {
+     const weatherInfo = new Weather(obj);
+     //forecast.push(weatherInfo);
+     return weatherInfo;
+    })
+     //console.log(result);
+     response.send(result);
+ 
+   });
+ };
 
 
 
