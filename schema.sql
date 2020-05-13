f0a726c33078fc0c125d8d721da9a07759fa6363
d-d-wolfe <dave.wolfe623@gmail.com>
2020-05-13 15:52:02 -0700

    completed the database and table creation

---
+++ schema.sql
@@ -0,0 +1,9 @@
+DROP TABLE city_info;
+
+CREATE TABLE city_info (
+  id SERIAL PRIMARY KEY,
+  search_query VARCHAR(255),
+  latitude NUMERIC,
+  longitude NUMERIC,
+  formatted_query VARCHAR(255)
+)
